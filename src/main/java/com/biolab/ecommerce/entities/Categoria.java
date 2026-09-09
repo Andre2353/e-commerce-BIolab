537d3b447f8044ed14371e7525663b4674abb3e3
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-09 08:46:10 -0300

    feat: set adicionado ao categoria

--- src/main/java/com/biolab/ecommerce/entities/Categoria.java
+++ src/main/java/com/biolab/ecommerce/entities/Categoria.java
@@ -5,6 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "categoria")
 @Data
@@ -15,4 +18,7 @@ public class Categoria {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
+    @ManyToMany(mappedBy = "categoria")
+    // a diferença do list para o set é que o set não permite duplicada
+    private Set<Produto> produto = new HashSet<>();
 }
