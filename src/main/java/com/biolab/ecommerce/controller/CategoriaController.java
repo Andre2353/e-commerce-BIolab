112e2d50366e2ecbaff9d59ce4b85cf7bf3dc074
Andre2353 <andrepassosdossantos64@gmail.com>
2026-09-16 09:13:08 -0300

    feat: adicionando o controller categorias

--- src/main/java/com/biolab/ecommerce/controller/CategoriaController.java
+++ src/main/java/com/biolab/ecommerce/controller/CategoriaController.java
@@ -1,28 +1,52 @@
 package com.biolab.ecommerce.controller;
 
 import com.biolab.ecommerce.DTOs.CategoriaRequest;
-import com.biolab.ecommerce.repository.CategoriaRepository;
+import com.biolab.ecommerce.DTOs.CategoriaResponse;
+import com.biolab.ecommerce.DTOs.ProdutoRequest;
 import com.biolab.ecommerce.service.CategoriaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("categoria")
+@RequestMapping("/categorias")
 public class CategoriaController {
-    private final CategoriaService Service;
 
-    public CategoriaController(CategoriaService service) {
-        Service = service;
+    private final CategoriaService categoriaService;
+
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
     }
-
 
     @PostMapping
-    public ResponseEntity<?> criarcat(@RequestBody CategoriaRequest dto){
-        return  ResponseEntity.status(HttpStatus.CREATED).body((Service.criarCategoria(dto)));
+    public ResponseEntity<String> criarCategoria(@RequestBody CategoriaRequest request) {
+        String resposta = categoriaService.criarCategoria(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaResponse>> listarCategorias() {
+        List<CategoriaResponse> categorias = categoriaService.Listarcategoria();
+        return ResponseEntity.ok(categorias);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaResponse> buscarPorId(@PathVariable Long id) {
+        CategoriaResponse categoria = categoriaService.buscarpoid(id);
+        return ResponseEntity.ok(categoria);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoriaResponse> atualizarCategoria(@PathVariable Long id, @RequestBody ProdutoRequest request) {
+        CategoriaResponse categoriaAtualizada = categoriaService.atualizarid(id, request);
+        return ResponseEntity.ok(categoriaAtualizada);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletarCategoria(@PathVariable Long id) {
+        String resposta = categoriaService.deletar(id);
+        return ResponseEntity.ok(resposta);
     }
 }
-
